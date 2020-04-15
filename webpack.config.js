a84ad79c0a872dd6878602ec3c1cdce95d39a76e
ozkn <ozkn666@yahoo.co.jp>
2020-04-15 10:22:47 +0900

    browsersyncの設定を追加

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,8 @@ const path = require('path');
 // cssファイル吐き出す版を使う時に読み込むプラグイン
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+
 module.exports = {
   mode: MODE,
   entry: './src/index.js',
@@ -72,6 +74,18 @@ module.exports = {
     // cssファイル吐き出す版を使う時の設定
     new MiniCssExtractPlugin({
       filename: './css/style.css'
+    }),
+    new BrowserSyncPlugin({
+      // browse to http://localhost:3000/ during development,
+      // ./public directory is being served
+      host: 'localhost',
+      port: 3000,
+      server: { baseDir: ['public'] },
+      files: [
+        "src",
+        "public/index.html"
+      ]
     })
+
   ]
 };
