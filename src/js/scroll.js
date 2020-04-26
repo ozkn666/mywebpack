4827bf54735f96ccce5b972855c10c64ecbd0e70
kenta.ozawa <ozkn666@yahoo.co.jp>
2020-04-26 11:28:31 +0900

    スクロール実行時にメニューを閉じる動作を追加

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,11 +1,15 @@
 export default function () {
   $(function () {
+    const menu = $('.p-hamburger-menu');
+    const main = $('.l-main');
     $('a[href^="#"]').click(function() {
-      var speed = 1000;
-      var href= $(this).attr("href");
-      var target = $(href == "#" || href == "" ? 'html' : href);
-      var position = target.offset().top;
+      let speed = 1000;
+      let href= $(this).attr("href");
+      let target = $(href == "#" || href == "" ? 'html' : href);
+      let position = target.offset().top;
       $('body,html').animate({scrollTop:position}, speed, 'swing');
+      menu.toggleClass('p-hamburger-menu__open');
+      main.toggleClass('p-hamburger-btn__open');
       return false;
     });
   })
