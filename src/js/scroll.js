a4afaaad662c3e008a73fd999baf5708a05084a7
kenta.ozawa <ozkn666@yahoo.co.jp>
2020-04-27 09:52:30 +0900

    ページトップのバグを修正

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -5,9 +5,13 @@ export default function () {
     $('a[href^="#"]').click(function() {
       let speed = 1000;
       let href= $(this).attr("href");
+      let className = $(this).attr("class");
       let target = $(href == "#" || href == "" ? 'html' : href);
       let position = target.offset().top;
       $('body,html').animate({scrollTop:position}, speed, 'swing');
+      if (className === "p-page-top") {
+        return false;
+      }
       menu.toggleClass('p-hamburger-menu__open');
       main.toggleClass('p-hamburger-btn__open');
       return false;
